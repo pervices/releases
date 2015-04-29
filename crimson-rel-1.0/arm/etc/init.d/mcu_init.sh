4ae25b68801d4b807852ff4fae5b5975334d81c4
Howard Pang <howard.p@pervices.com>
2015-04-29 21:06:23 +0000

    v1.0: removed old website initialization

--- crimson-rel-1.0/arm/etc/init.d/mcu_init.sh
+++ crimson-rel-1.0/arm/etc/init.d/mcu_init.sh
@@ -2,15 +2,11 @@
 
 case "$1" in
   start)
-        uart-app "board -l 5"
-        uart-app "fpga -o 1"
-	cp /www/pages/dig/state_default.xml /www/pages/dig/state_current.xml
-	cp /www/pages/rx/state_default.xml /www/pages/rx/state_current.xml
-	cp /www/pages/tx/state_default.xml /www/pages/tx/state_current.xml
-	cp /www/pages/synth/state_default.xml /www/pages/synth/state_current.xml
+   uart-app "board -l 5"
+   uart-app "fpga -o 1"
 	ifconfig eth0 up
-        ifconfig eth0 192.168.10.2
-        ethtool -s eth0 speed 100 duplex full autoneg off
+   ifconfig eth0 192.168.10.2
+   ethtool -s eth0 speed 100 duplex full autoneg off
 	mem rw sys3 0x1
    cd /home/root/pv_web; node server.js &
    server &
