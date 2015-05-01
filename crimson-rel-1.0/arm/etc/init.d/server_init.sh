24e9b50f37cb474619ea3e619f59882055ff7902
Howard Pang <howard.p@pervices.com>
2015-05-01 15:40:06 +0000

    Reset script uses MCU command to reboot

--- crimson-rel-1.0/arm/etc/init.d/server_init.sh
+++ crimson-rel-1.0/arm/etc/init.d/server_init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 cd /home/root/pv_web
+counter = 0
 until node server.js; do
 	echo "Crimson NodeJS crashed with exit code $?. Respawning..." > /home/root/node_crash.log
 	sleep 1
+   counter=$((counter+1))
+   if [[ "$counter" -gt 5 ]]; then
+      echo "Crimson NodeJS crashed repeatedly. Rebooting Crimson device..." > /home/root/node_crash.log
+      echo "fpga -r" | mcu
+   fi
 done
