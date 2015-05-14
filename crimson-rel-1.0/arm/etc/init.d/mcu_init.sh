dac6556f6f23d39c40a11309ac70c9aa2b5d7e96
Howard Pang <howard.p@pervices.com>
2015-05-14 21:11:49 +0000

    v1.0: tuned startup scripts for usability
    - descriptive LED's
    - allows NodeJS app to crash 10 times

--- crimson-rel-1.0/arm/etc/init.d/mcu_init.sh
+++ crimson-rel-1.0/arm/etc/init.d/mcu_init.sh
@@ -7,9 +7,11 @@ case "$1" in
         ifconfig eth0 up
         ifconfig eth0 192.168.10.2
         ethtool -s eth0 speed 100 duplex full autoneg off
-        mem rw sys3 0x1
-	server &
+        mem rw sys4 0x3
+        server &
         /etc/init.d/server_init.sh &
+        mem rw sys3 0x1
+        mem rw sys4 0x0
 	;;
   stop)
 	;;
