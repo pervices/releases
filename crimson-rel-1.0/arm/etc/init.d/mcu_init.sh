36831dd6009beb480b95d495900c455af7d5ce4a
Howard Pang <howard.p@pervices.com>
2015-09-03 21:43:21 +0000

    Release version for 1.0. New branch.

--- crimson-rel-1.0/arm/etc/init.d/mcu_init.sh
+++ crimson-rel-1.0/arm/etc/init.d/mcu_init.sh
@@ -2,14 +2,14 @@
 
 case "$1" in
   start)
-        uart-app "board -l 5"
         uart-app "fpga -o 1"
         ifconfig eth0 up
         ifconfig eth0 192.168.10.2
         ethtool -s eth0 speed 100 duplex full autoneg off
         mem rw sys4 0x3
-        server -d > /home/root/uhd.log &
+        server -d &
         /etc/init.d/server_init.sh &
+	sleep 15
         mem rw sys3 0x1
         mem rw sys4 0x0
 	;;
--- crimson-rel-1.0/arm/etc/rc5.d/S70mcu_init.sh
+++ crimson-rel-1.0/arm/etc/rc5.d/S70mcu_init.sh
@@ -2,14 +2,16 @@
 
 case "$1" in
   start)
-        uart-app "board -l 5"
         uart-app "fpga -o 1"
         ifconfig eth0 up
         ifconfig eth0 192.168.10.2
         ethtool -s eth0 speed 100 duplex full autoneg off
-        mem rw sys3 0x1
-	server &
+        mem rw sys4 0x3
+        server -d &
         /etc/init.d/server_init.sh &
+	sleep 15
+        mem rw sys3 0x1
+        mem rw sys4 0x0
 	;;
   stop)
 	;;
