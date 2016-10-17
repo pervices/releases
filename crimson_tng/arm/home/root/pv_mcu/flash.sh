9106ed6485af7ad301c38a00547fc3f428794d00
Namburi Raghuram <namburi.r@pervices.com>
2016-10-17 22:28:27 +0000

    Updated crimson TNG MCU hex files to the latest

--- crimson_tng/arm/home/root/pv_mcu/flash.sh
+++ crimson_tng/arm/home/root/pv_mcu/flash.sh
@@ -2,14 +2,14 @@
 
 if [ $# -lt 1 ]
 then
-	echo "Usage : $0 dig | rx | tx | synth | all"
+	echo "Usage : $0 rx | tx | synth | all"
 	exit
 fi
 
-if [ "$1" != 'synth' ] && [ "$1" != 'tx' ] && [ "$1" != 'rx' ] && [ "$1" != 'dig' ] && [ "$1" != 'all' ]
+if [ "$1" != 'synth' ] && [ "$1" != 'tx' ] && [ "$1" != 'rx' ] && [ "$1" != 'all' ]
 then
 	echo "- invalid argument"
-	echo "- Usage : $0 dig | rx | tx | synth | all"	
+	echo "- Usage : $0 rx | tx | synth | all"	
 	exit
 fi
 
@@ -17,13 +17,12 @@ fi
 if [ "$1" = 'synth' ] || [ "$1" = 'all' ]
 then
 	echo "-  Programming the synth board MCU..."
-	echo -e "switch -s\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "boot -e\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttyS1
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttyS1 -b 115200 -e -U flash:w:synth.hex
-	echo -e "exit\r" > /dev/ttyS1
+	#usleep 300000
+	echo -e "boot -e\r" > /dev/ttycrimson-time
+	usleep 5500000
+	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttycrimson-time
+	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttycrimson-time -b 115200 -e -U flash:w:synth.hex
+	echo -e "exit\r" > /dev/ttycrimson-time
 	echo "- Done programming."
 	usleep 300000
 fi
@@ -31,13 +30,12 @@ fi
 if [ "$1" = 'rx' ] || [ "$1" = 'all' ]
 then
 	echo "-  Programming the rx board MCU..."
-	echo -e "switch -r\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "boot -e\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttyS1
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttyS1 -b 115200 -e -U flash:w:rx.hex
-	echo -e "exit\r" > /dev/ttyS1
+	#usleep 300000
+	echo -e "boot -e\r" > /dev/ttycrimson-rx
+	usleep 5500000
+	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttycrimson-rx
+	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttycrimson-rx -b 115200 -e -U flash:w:rx.hex
+	echo -e "exit\r" > /dev/ttycrimson-rx
 	echo "- Done programming."
 	usleep 4000000
 fi
@@ -45,23 +43,12 @@ fi
 if [ "$1" = 'tx' ] || [ "$1" = 'all' ]
 then
 	echo "-  Programming the tx board MCU..."
-	echo -e "switch -t\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "boot -e\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttyS1
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttyS1 -b 115200 -e -U flash:w:tx.hex
-	echo -e "exit\r" > /dev/ttyS1
+	#usleep 300000
+	echo -e "boot -e\r" > /dev/ttycrimson-tx
+	usleep 5500000
+	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttycrimson-tx
+	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttycrimson-tx -b 115200 -e -U flash:w:tx.hex
+	echo -e "exit\r" > /dev/ttycrimson-tx
 	echo "- Done programming."
 	usleep 300000
-fi
-
-if [ "$1" = 'dig' ] || [ "$1" = 'all' ]
-then
-	echo "-  Programming the digital board MCU..."
-	echo -e "boot -e\r" > /dev/ttyS1
-	usleep 300000
-	echo -e "abcdefghijklmnopqrstuvwxyz\r" > /dev/ttyS1
-	avrdude -c avr109 -B 8 -p x256a3u -P /dev/ttyS1 -b 115200 -e -U flash:w:dig.hex
-	echo "- Done programming. Please wait a few minutes for eLinux to boot-up."
 fi
