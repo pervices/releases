29b477e0d76d0c1b49ca662985994062ecf806d1
Daniel McLean <daniel.m@pervices.com>
2018-04-19 15:13:39 +0000

    Removing stored crimson host key

--- validateTNG.sh
+++ validateTNG.sh
@@ -1,13 +1,21 @@
 #update repository
 git pull origin ops/val/master
 
+#remove host ssh
+sed -i '/192.168.10.2/d' ~/.ssh/known_hosts
+
 echo "rtm5 or rtm4?: (5,4)"
 read isrtm5
-if [[ -z "${isrtm5// }" ]] || [[ "$isrtm5" = "5" ]] || [[ "$isFlash" = "rtm5" ]] || [[ "$isFlash" = "RTM5" ]]
+if [[ "$isrtm5" -eq "5" ]] || [[ "$isrtm5" = "rtm5" ]] || [[ "$isrtm5" = "RTM5" ]]
 then
+    echo "Validating RTM5"
     sh validateRTM5.sh
-elif [[ "$isFlash" = "4" ]] || [[ "$isFlash" = "rtm4" ]] || [[ "$isFlash" = "RTM4" ]]
+elif [[ "$isrtm5" -eq  "4" ]] || [[ "$isrtm5" = "rtm4" ]] || [[ "$isrtm5" = "RTM4" ]]
 then
+    echo "Validating RTM4"
     sh validateRTM4.sh
+else
+    echo "No valit selection, please type '5,rtm5,RTM5' for validationg rtm5 OR '4,rtm4,RTM4' for rtm4"
+
 fi
 
