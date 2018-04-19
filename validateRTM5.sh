811cf354936665425194d57e6066ed907fa616ab
Daniel McLean <daniel.m@pervices.com>
2018-04-19 15:55:00 +0000

    Fixing typo in scp command

--- validateRTM5.sh
+++ validateRTM5.sh
@@ -23,7 +23,7 @@ read isFlash
 if [[ -z "${isFlash// }" ]] || [[ "$isFlash" = "y" ]] || [[ "$isFlash" = "Y" ]] || [[ "$isFlash" = "yes" ]]
 then
     echo "Updating unit"
-    scp updateCrimsonRtm5 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@192.168.10.2:~/ 
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no updateCrimsonRtm5 root@192.168.10.2:~/ 
     
     if [ -z "$1" ]
     then
