f97ae87649078e2fa4383f65c02cab32ff3a4482
Daniel McLean <daniel.m@pervices.com>
2018-04-20 15:31:31 +0000

    Fixing various typos

--- cleanSDCard.sh
+++ cleanSDCard.sh
@@ -7,7 +7,15 @@
 
  if [ -z "$1" ]
 then
-    sh validateRTM5.sh
+    ssh root@192.168.10.2  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '\
+        rm -rf /home/dev0/*; \
+        rm -rf /home/root/*; \
+        rm -rf /var/crimson/history; \
+        rm -rf /var/crimson/calibration-data; \
+        rm -rf /var/log/journal/*
+        sync
+        exit\
+        '
 elif [ "$1" = "nolut" ]
 then
     ssh root@192.168.10.2  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '\
