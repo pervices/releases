a0f87aca4842d0aa849bc1fc98aac50f592c0d4e
John Polak <john.p@pervices.com>
2020-09-10 14:51:39 +0000

    CRIMSON,RTM8: validator to entirly replace jounrald.conf rather than editing with sed

--- crimson-rtm8/update-journal-size.sh
+++ crimson-rtm8/update-journal-size.sh
@@ -1,8 +1,3 @@
 echo "Updating journal log size"
-sed -i 's/#Storage=auto/Storage=volatile/g'  /etc/systemd/journald.conf 
-sed -i 's/#Storage=persistent/Storage=volatile/g' /etc/systemd/journald.conf
-sed -i 's/Storage=auto/Storage=volatile/g' /etc/systemd/journald.conf
-sed -i 's/Storage=persistent/Storage=volatile/g' /etc/systemd/journald.conf
-sed -i 's/#RuntimeMaxUse=/RuntimeMaxUse=100M/g' /etc/systemd/journald.conf 
-sed -i 's/#RuntimeKeepFree=/RuntimeKeepFree=150M/g' /etc/systemd/journald.conf 
-sed -i 's/#RuntimeMaxFileSize=/RuntimeMaxFileSize=30M/g' /etc/systemd/journald.conf 
+rm /etc/systemd/journald.conf
+cp journald.conf /etc/systemd
