206066a9a0b08bf4769b8befb2b9fd9de0c631e6
John Polak <john.p@pervices.com>
2023-08-14 22:02:07 +0000

    SCRIPT: check that update is being run an SDR

--- crimson-rtm6/update-sd.sh
+++ crimson-rtm6/update-sd.sh
@@ -1,3 +1,10 @@
+if uname -m | grep -v armv7l
+then
+	echo "ERROR: It appears you're running this update package on a host machine."
+	echo "Please use scp to copy the update package onto a Crimson SDR."
+	exit 1
+fi
+
 sh create_ramdisk.sh;sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh;sh update-mcu.sh; sh update-server.sh ; sh update-fpga-sd.sh ; echo "Waiting 2mins for server to come up"; sleep 120; sh checkVersion.sh; 
 if [ -z "$1" ]
   then
