4733d4ed28010c2439721e6662fb52afbe5cd113
Victor Wollesen <victor.w@pervices.com>
2024-05-02 21:57:00 +0000

    UPDATE-SD: Automatically reboot after LUT generation.

--- crimson-rtm7/update-sd.sh
+++ crimson-rtm7/update-sd.sh
@@ -8,10 +8,10 @@ fi
 sh create_ramdisk.sh;sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh;sh update-mcu.sh; sh update-server.sh ; sh update-fpga-sd.sh ; echo "Waiting 2mins for server to come up"; sleep 120; sh checkVersion.sh; 
 if [ -z "$1" ]
   then
-    sh adf-lut.sh; 
+    sh adf-lut.sh && sync && reboot; 
   elif [ "$1" = "nolut" ]; then
     exit
   else
-    sh adf-lut.sh; 
+    sh adf-lut.sh && sync && reboot; 
 fi
 
--- crimson-rtm8/update-sd.sh
+++ crimson-rtm8/update-sd.sh
@@ -8,12 +8,12 @@ fi
 if [ -z "$1" ]
   then
     sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting 1min for server to come up"; sleep 60; sh checkVersion.sh;
-    sh adf-lut.sh;
+    sh adf-lut.sh && sync && reboot;
   elif [ "$1" = "nolut" ]; then
     sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting 1min for server to come up"; sleep 60; sh checkVersion.sh;
   elif [ "$1" = "onlylut" ]; then
-    sh adf-lut.sh;
+    sh adf-lut.sh && sync && reboot;
   else
     sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting 1min for server to come up"; sleep 60; sh checkVersion.sh;
-    sh adf-lut.sh;
+    sh adf-lut.sh && sync && reboot;
 fi
