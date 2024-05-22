3aee8891c93cc3d23969b57caca3015fa63ac0c4
Victor Wollesen <victor.w@pervices.com>
2024-05-22 17:25:17 +0000

    AUTOREBOOT: Reboot after 3h delay as LUT gen is non-blocking.

--- crimson-rtm8/update-sd.sh
+++ crimson-rtm8/update-sd.sh
@@ -8,12 +8,12 @@ fi
 if [ -z "$1" ]
   then
     sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting 1min for server to come up"; sleep 60; sh checkVersion.sh;
-    sh adf-lut.sh && sync && reboot;
+    sh adf-lut.sh && sync && sleep 10800 && reboot;
   elif [ "$1" = "nolut" ]; then
     sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting 1min for server to come up"; sleep 60; sh checkVersion.sh;
   elif [ "$1" = "onlylut" ]; then
-    sh adf-lut.sh && sync && reboot;
+    sh adf-lut.sh && sync && sleep 10800 && reboot;
   else
     sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting 1min for server to come up"; sleep 60; sh checkVersion.sh;
-    sh adf-lut.sh && sync && reboot;
+    sh adf-lut.sh && sync && sleep 10800 && reboot;
 fi
