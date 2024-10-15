6bbefa0be72dc88e2dc171acbbdc966c529ce211
Victor Wollesen <victor.w@pervices.com>
2024-10-15 19:50:54 +0000

    UPDATE-SD: Fix reboot routine.

--- crimson-rtm10/update-sd.sh
+++ crimson-rtm10/update-sd.sh
@@ -7,13 +7,11 @@ fi
 
 if [ -z "$1" ]
   then
-    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; echo "Waiting for MCUs to boot"; sleep 25; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 25; sh checkVersion.sh;
-    sh adf-lut.sh && sync && sleep 10800 && reboot;
+    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sync; sh checkVersion.sh; sh adf-lut.sh; sleep 10800; sync && reboot;
   elif [ "$1" = "nolut" ]; then
-    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; echo "Waiting for MCUs to boot"; sleep 25; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 25; sh checkVersion.sh;
+    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30;sh checkVersion.sh;
   elif [ "$1" = "onlylut" ]; then
-    sh adf-lut.sh && sync && sleep 10800 && reboot;
+    sh adf-lut.sh; sleep 10800; sync && reboot;
   else
-    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; echo "Waiting for MCUs to boot"; sleep 25; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 25; sh checkVersion.sh;
-    sh adf-lut.sh && sync && sleep 10800 && reboot;
+    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sync; sh checkVersion.sh; sh adf-lut.sh; sleep 10800; sync && reboot;
 fi
--- crimson-rtm7/update-sd.sh
+++ crimson-rtm7/update-sd.sh
@@ -5,13 +5,13 @@ then
 	exit 1
 fi
 
-sh create_ramdisk.sh;sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh;sh update-mcu.sh; sh update-server.sh ; sh update-fpga-sd.sh ; echo "Waiting 2mins for server to come up"; sleep 120; sh checkVersion.sh; 
+sh create_ramdisk.sh;sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh;sh update-mcu.sh; sh update-server.sh ; sh update-fpga-sd.sh ; echo "Waiting 2mins for server to come up"; sleep 120; sync; sh checkVersion.sh;
 if [ -z "$1" ]
   then
-    sh adf-lut.sh && sync && sleep 10800 && reboot; 
+    sh adf-lut.sh; sleep 10800; sync && reboot;
   elif [ "$1" = "nolut" ]; then
     exit
   else
-    sh adf-lut.sh && sync && sleep 10800 && reboot; 
+    sh adf-lut.sh; sleep 10800; sync && reboot;
 fi
 
