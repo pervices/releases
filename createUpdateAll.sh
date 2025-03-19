af15a07e47be057a12035d596ae619df031adf15
John Polak <john.p@pervices.com>
2025-03-19 19:13:17 +0000

    SCRIPT: createUpdateAll add 300MSPS and RTM12

--- createUpdateAll.sh
+++ createUpdateAll.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #This is a wrapper to quickly generate all the updates;
-rm updateCrimsonRtm10-325msps updateCrimsonRtm4-325msps updateCrimsonRtm5-325msps updateCrimsonRtm6-325msps updateCrimsonRtm7-325msps updateCrimsonRtm8-325msps updateCrimsonRtm9-325msps
+rm updateCrimsonRtm*
 ./createUpdate.sh crimson-rtm6-325msps/ updateCrimsonRtm6-325msps && \
 ./createUpdate.sh crimson-rtm7-325msps/ updateCrimsonRtm7-325msps && \
 ./createUpdate.sh crimson-rtm8-325msps/ updateCrimsonRtm8-325msps && \
 ./createUpdate.sh crimson-rtm9-325msps/ updateCrimsonRtm9-325msps && \
-./createUpdate.sh crimson-rtm10-325msps/ updateCrimsonRtm10-325msps
+./createUpdate.sh crimson-rtm10-325msps/ updateCrimsonRtm10-325msps && \
+./createUpdate.sh crimson-rtm10-300msps/ updateCrimsonRtm10-300msps && \
+./createUpdate.sh crimson-rtm12-325msps/ updateCrimsonRtm12-325msps && \
+./createUpdate.sh crimson-rtm12-300msps/ updateCrimsonRtm12-300msps
