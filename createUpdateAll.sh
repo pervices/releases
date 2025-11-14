add000640889674f7a8736722433fe9641ed4766
John Polak <john.p@pervices.com>
2025-11-14 20:14:55 +0000

    SCRIPT: create all includes rtm15 and updated to new naming convention

--- createUpdateAll.sh
+++ createUpdateAll.sh
@@ -2,11 +2,13 @@
 
 #This is a wrapper to quickly generate all the updates;
 rm updateCrimsonRtm*
-./createUpdate.sh crimson-rtm6-325msps/ updateCrimsonRtm6-325msps && \
-./createUpdate.sh crimson-rtm7-325msps/ updateCrimsonRtm7-325msps && \
-./createUpdate.sh crimson-rtm8-325msps/ updateCrimsonRtm8-325msps && \
-./createUpdate.sh crimson-rtm9-325msps/ updateCrimsonRtm9-325msps && \
-./createUpdate.sh crimson-rtm10-325msps/ updateCrimsonRtm10-325msps && \
-./createUpdate.sh crimson-rtm10-300msps/ updateCrimsonRtm10-300msps && \
-./createUpdate.sh crimson-rtm12-325msps/ updateCrimsonRtm12-325msps && \
-./createUpdate.sh crimson-rtm12-300msps/ updateCrimsonRtm12-300msps
+./createUpdate.sh crimson-rtm6-325msps/ update-crimson-rtm6-325msps && \
+./createUpdate.sh crimson-rtm7-325msps/ update-crimson-rtm7-325msps && \
+./createUpdate.sh crimson-rtm8-325msps/ update-crimson-rtm8-325msps && \
+./createUpdate.sh crimson-rtm9-325msps/ update-crimson-rtm9-325msps && \
+./createUpdate.sh crimson-rtm10-325msps/ update-crimson-rtm10-325msps && \
+./createUpdate.sh crimson-rtm10-300msps/ update-crimson-rtm10-300msps && \
+./createUpdate.sh crimson-rtm12-325msps/ update-crimson-rtm12-325msps && \
+./createUpdate.sh crimson-rtm12-300msps/ update-crimson-rtm12-300msps && \
+./createUpdate.sh crimson-rtm15-325msps/ update-crimson-rtm15-335msps && \
+./createUpdate.sh crimson-rtm15-300msps/ update-crimson-rtm15-300msps
