a6d6a3f372e990feddb42e9a4f950a2ff20008a8
John Polak <john.p@pervices.com>
2025-11-14 20:07:37 +0000

    CRIMSON,RTM12: remove reboot that interrupted the update sequence

--- crimson-rtm12-300msps/update-sd.sh
+++ crimson-rtm12-300msps/update-sd.sh
@@ -7,11 +7,11 @@ fi
 
 if [ -z "$1" ]
   then
-    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sync; sh checkVersion.sh; sh adf-lut.sh; sleep 10800; sync && reboot;
+    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sync; sh checkVersion.sh; sh adf-lut.sh;
   elif [ "$1" = "nolut" ]; then
-    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30;sh checkVersion.sh;
+    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sh checkVersion.sh;
   elif [ "$1" = "onlylut" ]; then
-    sh adf-lut.sh; sleep 10800; sync && reboot;
+    sh adf-lut.sh;
   else
-    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sync; sh checkVersion.sh; sh adf-lut.sh; sleep 10800; sync && reboot;
+    sh create_ramdisk.sh; sh update-journal-size.sh; sh update-fs.sh; sh update-website.sh; sh update-mcu.sh; sh update-server.sh; sh update-fpga-sd.sh; echo "Waiting for server to come up"; sleep 30; sync; sh checkVersion.sh; sh adf-lut.sh;
 fi
