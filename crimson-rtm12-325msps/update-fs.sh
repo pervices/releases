c32cc2d40ba5abfa0eda9ba6f628457f5a5a1fac
John Polak <john.p@pervices.com>
2025-12-04 16:26:02 +0000

    CRIMSON,UPDATE,FS: suppress systemctl warning unless necessary
    also give the server.service more time to start up to avoid
    warnings/errors during update

--- crimson-rtm12-325msps/update-fs.sh
+++ crimson-rtm12-325msps/update-fs.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+echo Updating File System
+
+# avoid warning about updating the service if it not actually changed
+if diff fs/lib/systemd/system/crimson-server.service /lib/systemd/system/crimson-server.service > /dev/null;
+then
+    # the files match so no need to copy it over, remove it from the update directory
+    echo server.service already up to date
+    rm fs/lib/systemd/system/crimson-server.service
+fi
+
 cp -R fs/* /
+
+# if any systemd files changed, we need to reload
+systemctl daemon-reload
 
 # need to install the flash utility from the firmware repo before we try to flash the MCU
 cp firmware/flash /usr/bin/
